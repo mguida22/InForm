dc582b6568cc53194908d6c8b7eaaf7d3cd9975d
mguida22 <michael.guida@colorado.edu>
2015-03-15 22:07:34 -0600

    Logic for checking positions

--- SensorShirt/SensorShirt.ino
+++ SensorShirt/SensorShirt.ino
@@ -2,13 +2,38 @@
   Sensor Shirt
   Kylie Dale and Michael Guida
  */
+const int backPin = 0;
+const int shoulderPin = 1;
+const int underarmPin = 2;
+
+int backValue = null;
+int shoulderValue = null;
+int underarmValue = null;
+
+String pos = "Plank";
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
+  backValue = analogRead(backPin);
+  shoulderValue = analogRead(shoulderPin);
+  underarmValue = analogRead(underarmValue);
+  
+  println(checkPosition());
+  
   delay(300);
+}
+
+boolean checkPosition(){
+  case(pos) {
+    case "Plank":
+      if (armFwdRight90() && backStraight()){
+        return true;
+      } else { return false; }
+    default:
+      println("Invalid Position");
+      break;
+  }
 }
