6ed43216b56dddf794f2bc48aa2b3f8b6f53d7d2
kyliedale <kylie.dale@colorado.edu>
2015-03-15 21:54:14 -0600

    made functions in PositionFunctions

--- SensorShirt/PositionFunctions.ino
+++ SensorShirt/PositionFunctions.ino
@@ -1,3 +1,52 @@
 /*
-  Helper functions for position
+  Position Functions
+  Kylie Dale and Michael Guida
 */
+
+boolean armAtSideRight() {
+  
+}
+
+boolean armFwdRight45() {
+  
+}
+
+boolean armFwdRight90() {
+  
+}
+
+boolean armFwdRight135() {
+  
+}
+
+boolean armFwdRight180() {
+  
+}
+
+boolean armSideRight45() {
+  
+}
+
+boolean armSideRight90() {
+  
+}
+
+boolean armSideRight135() {
+  
+}
+
+boolean armSideRight180() {
+  
+}
+
+boolean backStraight() {
+  
+}
+
+boolean backBentFwd() {
+  
+}
+
+boolean backBentBack() {
+  
+}
--- SensorShirt/SensorShirt.ino
+++ SensorShirt/SensorShirt.ino
@@ -10,5 +10,6 @@ void setup() {
 void loop() {
   int sensorValue = analogRead(A0);
   Serial.println(sensorValue);
+  printHi();
   delay(300);
 }
