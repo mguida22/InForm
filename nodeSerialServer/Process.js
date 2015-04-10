9d7004ff4c0493681c8336965a3583dc9aed190b
mguida22 <michael.guida@colorado.edu>
2015-04-10 12:57:32 -0600

    Builds JSON objects from selected pose

--- nodeSerialServer/Process.js
+++ nodeSerialServer/Process.js
@@ -3,6 +3,46 @@
  * Kylie Dale and Michael Guida
  */
 
+//Builds JSON object from selected pose
+//TODO-MG: send JSON to Arduino code
+function runPose() {
+	var currentPose = $('input[name="currentPose"]:checked').val();
+	console.log(currentPose);
+
+	if (currentPose == 'plank') {
+		pose = {
+			'name': 'plank',
+			'leftArm': 'fwd90',
+			'rightArm': 'fwd90',
+			'back': 'straight'
+		};
+	} else if (currentPose == 'mountainPose') {
+		pose = {
+			'name': 'mountainPose',
+			'leftArm': 'fwd180',
+			'rightArm': 'fwd180',
+			'back': 'straight'
+		};
+	} else if (currentPose == 'downwardDog') {
+		pose = {
+			'name': 'downwardDog',
+			'leftArm': 'fwd180',
+			'rightArm': 'fwd180',
+			'back': 'straight'
+		};
+	} else if (currentPose == 'warrior') {
+		pose = {
+			'name': 'warrior',
+			'leftArm': 'side90',
+			'rightArm': 'side90',
+			'back': 'straight'
+		};
+	}
+
+	console.log(pose);
+}
+
+/*
 // open a connection to the serial server:
 var socket = io.connect('http://localhost:8080');
 // when you get a serialdata event, do this:
@@ -11,3 +51,4 @@ socket.on('serialEvent', function (data) {
 	// whatever the 'value' property of the received data is:
 	textDisplay.innerHTML = data;
 });
+*/
