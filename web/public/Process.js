39959a4e2e1cd1e9b5f7f37da5ba047f6aea01fa
mguida22 <michael.guida@colorado.edu>
2015-04-14 00:49:26 -0600

    Display data nicely to user

--- web/public/Process.js
+++ web/public/Process.js
@@ -7,8 +7,37 @@
 socket.on('serialEvent', function (data) {
 	// Parse JSON and display name
 	data = JSON.parse(data);
-	textDisplay.innerHTML = data.name;
+	displayData(data);
 });
+
+//Displays status to user
+function displayData(data) {
+	data.rightArm = 1;
+	if (data.leftArm == 0) {
+		leftArmDisp.innerHTML = 'Incorrect Position';
+		document.getElementById("leftArmDisp").className = "label label-danger";
+	} else {
+		leftArmDisp.innerHTML = 'Correct Position';
+		document.getElementById("leftArmDisp").className = "label label-success";
+	}
+
+	if (data.rightArm == 0) {
+		rightArmDisp.innerHTML = 'Incorrect Position';
+		document.getElementById("rightArmDisp").className = "label label-danger";
+	} else {
+		rightArmDisp.innerHTML = 'Correct Position';
+		document.getElementById("rightArmDisp").className = "label label-success";
+	}
+
+	if (data.back == 0) {
+		backDisp.innerHTML = 'Incorrect Position';
+		document.getElementById("backDisp").className = "label label-danger";
+	} else {
+		backDisp.innerHTML = 'Correct Position';
+		document.getElementById("backDisp").className = "label label-success";
+	}
+}
+
 
 //Builds JSON object from selected pose
 function runPose() {
